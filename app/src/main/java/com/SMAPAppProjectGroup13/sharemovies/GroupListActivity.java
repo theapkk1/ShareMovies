265812d7d5bdf6504f73417303698f70583865f4
theapkk1 <thea.plenus.kjeldahl.kristensen@post.au.dk>
2020-04-25 16:49:07 +0200

    Layout mainActivity og authentication firebase

--- app/src/main/java/com/SMAPAppProjectGroup13/sharemovies/GroupListActivity.java
+++ app/src/main/java/com/SMAPAppProjectGroup13/sharemovies/GroupListActivity.java
@@ -1,12 +1,12 @@
 package com.SMAPAppProjectGroup13.sharemovies;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class GroupListActivity extends AppCompatActivity implements Adapter.OnMovieListener{
 
